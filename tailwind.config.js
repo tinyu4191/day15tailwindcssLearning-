f8e1dd09492e92daa53d4b0edb4768d69afeebf3
Terry <60589771+tinyu4191@users.noreply.github.com>
2022-02-08 17:16:11 +0800

    add mode jit & modify purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 module.exports = {
-    // mode: 'jit',
-    purge: {
-        enabled: true, // true: 開啟 / false: 關閉
-        content: ['./*.{html,js}', './**/*.{html,js}'],
-        options: {
-            safelist: [/(bg|ring)-(.*)-(\d{1}0{1,2})/],
-        },
-    },
+    mode: 'jit',
+    purge: ['./*.{html,js}', './**/*.{html,js}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
