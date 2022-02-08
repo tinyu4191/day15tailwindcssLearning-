8ceacaf917707205c9ccab023ce4ffd3f57cd262
Terry <60589771+tinyu4191@users.noreply.github.com>
2022-02-08 16:56:19 +0800

    add purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
     // mode: 'jit',
-    purge: ['./*.{html,js}', './**/*.{html,js}'],
+    purge: {
+        enabled: true, // true: 開啟 / false: 關閉
+        content: ['./*.{html,js}', './**/*.{html,js}'],
+        options: {
+            safelist: [/(bg|ring)-(.*)-(\d{1}0{1,2})/],
+        },
+    },
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
